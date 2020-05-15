1fb4279b1b9cfa7a12a3e7dfbf979b668552072b
Jason Meulenhoff <5448014+Plnda@users.noreply.github.com>
2020-05-15 23:55:06 +0200

    Update Bot.elm

--- implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
+++ implement/applications/eve-online/eve-online-mining-bot/src/Bot.elm
@@ -433,7 +433,7 @@ unlockTargetsNotForMining context =
     let
         targetsToUnlock =
             context.parsedUserInterface.targets
-                |> List.filter (.textsTopToBottom >> List.any (String.toLower >> String.contains "plagioclase") >> not)
+                |> List.filter (.textsTopToBottom >> List.any (String.toLower >> String.contains "asteroid") >> not)
     in
     targetsToUnlock
         |> List.head
